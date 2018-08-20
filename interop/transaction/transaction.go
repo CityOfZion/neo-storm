2fdb0c3f40ef1cc5dc77fcb125d7135ab18f08e2
Anthony De Meulemeester <anthony@academiclabs.co>
2018-08-20 20:58:21 +0200

    added draft of block and transaction interop api.

---
+++ interop/transaction/transaction.go
@@ -0,0 +1,7 @@
+package transaction
+
+// Package Transaction provides function signatures that can be used inside
+// smart contracts that are written in the neo-go-sc framework.
+
+// Transaction stubs a NEO transaction type.
+type Transaction interface{}
