80e1a47fec8fdc656900ad41c31bf7c306b7efe4
Anthony De Meulemeester <anthony@academiclabs.co>
2018-08-21 10:25:04 +0200

    added Runtime interop apis

--- interop/runtime/runtime.go
+++ interop/runtime/runtime.go
@@ -4,3 +4,42 @@ package runtime
 func CheckWitness(hash []byte) bool {
 	return true
 }
+
+// GetTrigger returns the smart contract invoke trigger which can be either
+// verification or application.
+func GetTrigger() byte {
+	return 0x00
+}
+
+// Application returns the application trigger.
+func Application() byte {
+	return 0x10
+}
+
+// Verification returns the verification trigger.
+func Verification() byte {
+	return 0x00
+}
+
+// Serialize serializes and item into a bytearray.
+func Serialize(item interface{}) []byte {
+	return nil
+}
+
+// Deserializes an item from a bytearray.
+func Deserialize(b []byte) interface{} {
+	return nil
+}
+
+// Log instucts the VM to log the given message.
+func Log(message string) {}
+
+// Notify an event to the VM.
+func Notify(arg interface{}) int {
+	return 0
+}
+
+// GetTime returns the timestamp of the most recent block.
+func GetTime() int {
+	return 0
+}
