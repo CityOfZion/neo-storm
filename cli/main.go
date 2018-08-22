eca3702d5c8a0dc38c544bcec75182dc4656ac05
Anthony De Meulemeester <anthony@academiclabs.co>
2018-08-22 19:12:56 +0200

    added simple testinvoke command to remote RPC nodes (#18)

--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/CityOfZion/neo-go/pkg/rpc"
 	"github.com/CityOfZion/neo-storm/compiler"
 	"github.com/urfave/cli"
 )
@@ -20,7 +26,7 @@ func main() {
 		{
 			Name:   "compile",
 			Usage:  "compile a smart contract to an .avm file",
-			Action: contactCompile,
+			Action: contractCompile,
 			Flags: []cli.Flag{
 				cli.StringFlag{
 					Name:  "in, i",
@@ -36,11 +42,22 @@ func main() {
 				},
 			},
 		},
+		{
+			Name:   "testinvoke",
+			Usage:  "testinvoke a smart contract against a remote NEO RPC node",
+			Action: contractTestInvoke,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "in, i",
+					Usage: "input file to be compiled",
+				},
+			},
+		},
 	}
 	ctl.Run(os.Args)
 }
 
-func contactCompile(ctx *cli.Context) error {
+func contractCompile(ctx *cli.Context) error {
 	src := ctx.String("in")
 	if len(src) == 0 {
 		return cli.NewExitError(errNoInput, 1)
@@ -54,6 +71,40 @@ func contactCompile(ctx *cli.Context) error {
 	if err := compiler.CompileAndSave(src, o); err != nil {
 		return cli.NewExitError(err, 1)
 	}
+
+	return nil
+}
+
+func contractTestInvoke(ctx *cli.Context) error {
+	src := ctx.String("in")
+	if len(src) == 0 {
+		return cli.NewExitError(errNoInput, 1)
+	}
+
+	b, err := ioutil.ReadFile(src)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	endpoint := "http://seed2.ngd.network:10332"
+	opts := rpc.ClientOptions{}
+	client, err := rpc.NewClient(context.TODO(), endpoint, opts)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	scriptHex := hex.EncodeToString(b)
+	resp, err := client.InvokeScript(scriptHex)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	b, err = json.MarshalIndent(resp.Result, "", "  ")
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+
+	fmt.Println(string(b))
 
 	return nil
 }
