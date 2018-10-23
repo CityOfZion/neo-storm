e17bd4f52f1072ed08394245a3886f2875ac5f7d
decentralisedkev <37423678+decentralisedkev@users.noreply.github.com>
2018-10-23 13:54:12 +0100

    fix bug for odd length array

--- compiler/emit.go
+++ compiler/emit.go
@@ -113,7 +113,7 @@ func arrayReverse(b []byte) []byte {
 		return b
 	}
 	dest := make([]byte, len(b))
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(b)-1; i < j+1; i, j = i+1, j-1 {
 		dest[i], dest[j] = b[j], b[i]
 	}
 	return dest
